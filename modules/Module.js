eec7f9f9372d363f70dbbc143cdc6b6d6559d634
BenjSta <61200235+BenjSta@users.noreply.github.com>
2020-12-20 16:43:07 +0100

    Update Module.js

--- modules/Module.js
+++ modules/Module.js
@@ -1,5 +1,12 @@
 class Module {
     constructor(name) {
-        this.name = name
+        this.name = name;
+        this.template_file = null;
+    }
+    render(target_element){
+        template = $.get(this.template_file);
+        html = Mustache.render(template, this);
+        target_element.append(html);
     }
 }
+
