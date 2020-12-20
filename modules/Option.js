26c93475e950af867e212dd8522fa7de684cd693
BenjSta <61200235+BenjSta@users.noreply.github.com>
2020-12-20 16:09:17 +0100

    Update Option.js

--- modules/Option.js
+++ modules/Option.js
@@ -1,6 +1,8 @@
 class Option{
-    constructor(name, text) {
+    constructor(name, text, normal_checked) {
         this.name = name;
         this.text = text;
+        this.normal_checked = normal_checked;
+        this.checked = false;
     }
 }
