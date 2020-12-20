33e3315ff3bdaf78c5fe9528a417ee5310440e34
Benjamin Stahl <benjamincosimostahl@hotmail.com>
2020-12-20 21:51:18 +0100

    path fixing

--- modu/GroupModule.js
+++ modu/GroupModule.js
@@ -2,7 +2,7 @@ import {Module} from "/MedAssistant/modu/Module.js";
 
 class GroupModule extends Module {
     constructor(name, modules, replacements, parent_element) {
-        super(name, "/modu/GroupModuleGUI.template.html", parent_element);
+        super(name, "/MedAssistant/modu/GroupModuleGUI.template.html", parent_element);
         this.modules = modules;
         this.replacements = replacements;
         this.collapsable = false;
