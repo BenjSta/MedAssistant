d5a04e053d86445cd201c49abd87f845dd4dc9e6
Jonathan Stahl <jonathanmarvinstahl@gmail.com>
2020-12-27 14:16:24 +0100

    test

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import {GroupModule} from "/MedAssistant/modu/GroupModule.js";
 var page_content = $("#page-content");
 var my_first_module = new GroupModule("Bewusstsein", [], [], page_content);
 my_first_module.render();
-var my_second_module = new GroupModule("Orientierung", [], [], page_content);
+var my_second_module = new GroupModule("Kognition", [], [], page_content);
 my_second_module.render();
 //example of adding elements to menu
 $("#forms").append('<li class="pure-menu-item"><a href="#" class="pure-menu-link">Psychopathologischer Befund</a></li>')
