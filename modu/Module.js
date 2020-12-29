94dde88f2fa2fa76230371093422b3853613672d
Benjamin Stahl <benjamincosimostahl@hotmail.com>
2020-12-29 19:37:17 +0100

    title page

--- modu/Module.js
+++ modu/Module.js
@@ -9,8 +9,12 @@ class Module {
         this.name = yaml_object.name;
 
         this.level = level
-
-        this.html_element = document.createElement('div');
+        if (level === 0){
+            this.html_element = document.createElement('div');
+        }
+        else{
+            this.html_element = document.createElement('span')
+        }
         this.html_element.classList.add("module");
         html_parent_element.appendChild(this.html_element);
         this.body_element = document.createElement('div');
@@ -19,14 +23,15 @@ class Module {
         if (level === 0) {
             this.heading_element = document.createElement("a");
             this.heading_element.classList.add("pure-button");
-            this.heading_element.classList.add("pure-button-heading");
+            this.heading_element.classList.add("module-heading");
             this.heading_element.classList.add("uppercase");
             this.heading_element.href = "#";
             this.heading_element.append(this.name);
             this.html_element.appendChild(this.heading_element);
             var self = this;
             this.heading_element.onclick = 
-                function(){self.toggleHideShowBody();};  
+                function(){self.toggleHideShowBody();}; 
+            this.body_element.style.display = "none";
         }
         else {
             this.heading_element = document.createElement("div");
